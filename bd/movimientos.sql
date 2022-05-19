751c5175dc22744ea6f3ec4158c9dfc6d7ab0a61
sotaax <damian.s263078@cesurformacion.com>
2022-05-19 14:33:33 +0200

    insert movimientos

---
+++ bd/movimientos.sql
@@ -0,0 +1,77 @@
+insert into movimientos values (01, 'normal', 'Puño certero', 15, null, null, null, null, 'ataque', -1);
+
+
+
+insert into movimientos values (02, 'volador', 'Garra de dragon', 8, null, null, null, null, 'ataque', 1);
+
+
+
+insert into movimientos values (03, 'agua', 'Hidropulso', 6, null, null, null, null, 'ataque', 1);
+
+
+
+insert into movimientos values (04, 'normal', 'Paz mental', null, 1, null, 'ataqueEspecial', 1, 'mejora', 1);
+
+
+
+insert into movimientos values (05, 'normal', 'Rugido', null, 1, null, 'defensaEspecial', 1, 'mejora', 1);
+
+
+
+insert into movimientos values (06, 'veneno', 'Tóxico', null, 3, 'VENENO', null, null, 'estado', 1);
+
+
+
+insert into movimientos values (07, 'normal', 'Corpulencia', null, 1, null, 'defensa', 1, 'mejora', 1);
+
+
+
+insert into movimientos values (08, 'planta', 'Recurrente', 1, null, null, null, null, 'ataque', 1);
+
+
+
+insert into movimientos values (09, 'fuego', 'Día soleado', null, 1, null, 'ataque', 1, 'mejora', 1);
+
+
+
+insert into movimientos values (10, 'normal', 'Hiperrayo', 15, null, null, null, null, 'ataque', -1);
+
+
+
+insert into movimientos values (11, 'agua', 'Danza lluvia', null, 1, null, 'ataque', 1, 'mejora', 1);
+
+
+
+insert into movimientos values (12, 'planta', 'Gigadrenado', 6, null, null, null, null, 'ataque', 1);
+
+
+
+insert into movimientos values (13, 'planta', 'Rayo solar', 15, null, null, null, null, 'ataque', -1);
+
+
+
+insert into movimientos values (14, 'electrico', 'Rayo', 9, null, null, null, null, 'ataque', 1);
+
+
+
+insert into movimientos values (15, 'tierra', 'Terremoto', 10, null, null, null, null, 'ataque', -1);
+
+
+
+insert into movimientos values (16, 'tierra', 'Excavar', 6, null, null, null, null, 'ataque', -1);
+
+
+
+insert into movimientos values (17, 'fuego', 'Lanzallamas', 9, null, null, null, null, 'ataque', 1);
+
+
+
+insert into movimientos values (18, 'bicho', 'Megacuerno', 12, null, null, null, null, 'ataque', -1);
+
+
+
+insert into movimientos values (19, 'bicho', 'Picadura', 6, null, null, null, null, 'ataque', -1);
+
+
+
+insert into movimientos values (20, 'volador', 'Vendaval', 11, null, null, null, null, 'ataque', 1);
