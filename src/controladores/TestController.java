401a180f57197fe3ab17afb0367a5358d66ad838
zuzask <josemiguel.s262263@cesurformacion.com>
2022-05-19 19:30:00 +0200

    Implemetacion portada

---
+++ src/controladores/TestController.java
@@ -0,0 +1,32 @@
+package controladores;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+
+public class TestController implements Initializable {
+
+    @FXML
+    private Button btnJUEGALECHAVAL;
+
+   @FXML
+    private void juegale(ActionEvent event){
+        
+    }
+
+    @FXML
+    private ImageView imagePortada;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        
+        
+    }
+    
+    
+}
