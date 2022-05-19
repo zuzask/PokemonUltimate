2eaddbdbeaded30a316f536187f40b08fef4bacc
zuzask <josemiguel.s262263@cesurformacion.com>
2022-05-19 19:30:19 +0200

    Implementacion portada

--- src/modelo/Main.java
+++ src/modelo/Main.java
@@ -1,19 +1,35 @@
 package modelo;
 
-public class Main {
+import java.io.IOException;
+import java.sql.*;
+import javax.sql.*;
 
-     public static void main(String[] args) {
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
+public class Main extends Application {
 
-        Pokemon pokemon = new Pokemon();
+    @Override
+    public void start(Stage primaryStage) {
+        Parent root;
+        try {
+            root = FXMLLoader.load(getClass().getResource("./vistas/VentanaPrincipal.fxml"));
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
+            primaryStage.show();
 
-        DbConexion.establecerConexion();
-
-        pokemon = Pokemon.generarPokemon();      
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         
-        DbConexion.cerrarConexion();
-        System.out.println(pokemon.mostrarPokemon());
     }
     
 }
