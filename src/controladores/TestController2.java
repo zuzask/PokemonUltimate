621f48e473f71b423b9879955c8e1bd3b0b0ab84
zuzask <josemiguel.s262263@cesurformacion.com>
2022-05-20 18:04:05 +0200

    Implementacion segunda ventana

---
+++ src/controladores/TestController2.java
@@ -0,0 +1,50 @@
+package controladores;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+public class TestController2 implements Initializable {
+
+
+    @FXML
+
+    private Button btnCapturar;
+
+    @FXML
+    private Button btnCombatir;
+
+    @FXML
+    private void combatir(ActionEvent event) throws IOException{
+
+    }
+
+    @FXML
+    private void capturar(ActionEvent event) throws IOException{
+
+    }
+
+    @FXML
+    private void mostrarEquipo(ActionEvent event) throws IOException{
+
+    }
+
+    @FXML
+    private void mostrarCaja(ActionEvent event) throws IOException{
+
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        
+        
+    }
+
+
+    
+}
