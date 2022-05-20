fb07cc7ecff8f3d6c645777dfc5a8bd373c8b786
zuzask <josemiguel.s262263@cesurformacion.com>
2022-05-20 18:04:18 +0200

    Implementacion segunda ventana

--- src/controladores/TestController.java
+++ src/controladores/TestController.java
@@ -1,32 +1,64 @@
 package controladores;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 public class TestController implements Initializable {
 
     @FXML
     private Button btnJUEGALECHAVAL;
 
-   @FXML
-    private void juegale(ActionEvent event){
-        
-    }
-
     @FXML
-    private ImageView imagePortada;
+    private Object stage;
+    
+
+   @FXML
+    private void juegale(ActionEvent event) throws IOException{
+
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("../vistas/SegundaVentana.fxml"));
+
+            Parent root = loader.load();
+
+            TestController2 controller = loader.getController();
+
+            Scene scene = new Scene(root);
+
+            Stage stage = new Stage();
+
+            stage.initModality(Modality.APPLICATION_MODAL);
+
+            stage.setScene(scene);
+
+            stage.showAndWait();
+
+            ((Stage) this.stage).close();
+
+
+        }
+
+    
+
+        
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         
         
     }
+    
     
     
 }
